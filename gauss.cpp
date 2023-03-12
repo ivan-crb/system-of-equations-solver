dd12cc6517d292c4efa01364186c90ed39c3735b
ivan-crb <guativan@gmail.com>
2023-03-12 17:32:47 +0100

    first commit

---
+++ gauss.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hell";
+
+
+
+
+}
+
+
+// https://github.com/ivan-crb/equation-system-solver.git
+// g++ gauss.cpp -o gauss; .\gauss
